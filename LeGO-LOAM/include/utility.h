ba9a74d5e20815b512208d5bedb703afdc53799f
code-fusheng <2561035977@qq.com>
2024-11-24 15:21:06 +0800

    fix : noetic

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -1,7 +1,6 @@
 #ifndef _UTILITY_LIDAR_ODOMETRY_H_
 #define _UTILITY_LIDAR_ODOMETRY_H_
 
-
 #include <ros/ros.h>
 
 #include <sensor_msgs/Imu.h>
@@ -10,7 +9,8 @@
 
 #include "cloud_msgs/cloud_info.h"
 
-#include <opencv/cv.h>
+// #include <opencv/cv.h>
+#include <opencv2/opencv.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -25,7 +25,7 @@
 
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_datatypes.h>
- 
+
 #include <vector>
 #include <cmath>
 #include <algorithm>
@@ -48,7 +48,7 @@
 
 using namespace std;
 
-typedef pcl::PointXYZI  PointType;
+typedef pcl::PointXYZI PointType;
 
 extern const string pointCloudTopic = "/velodyne_points";
 extern const string imuTopic = "/imu/data";
@@ -64,7 +64,7 @@ extern const int N_SCAN = 16;
 extern const int Horizon_SCAN = 1800;
 extern const float ang_res_x = 0.2;
 extern const float ang_res_y = 2.0;
-extern const float ang_bottom = 15.0+0.1;
+extern const float ang_bottom = 15.0 + 0.1;
 extern const int groundScanInd = 7;
 
 // HDL-32E
@@ -110,12 +110,11 @@ extern const int imuQueLength = 200;
 
 extern const float sensorMinimumRange = 1.0;
 extern const float sensorMountAngle = 0.0;
-extern const float segmentTheta = 60.0/180.0*M_PI; // decrese this value may improve accuracy
+extern const float segmentTheta = 60.0 / 180.0 * M_PI; // decrese this value may improve accuracy
 extern const int segmentValidPointNum = 5;
 extern const int segmentValidLineNum = 3;
 extern const float segmentAlphaX = ang_res_x / 180.0 * M_PI;
 extern const float segmentAlphaY = ang_res_y / 180.0 * M_PI;
-
 
 extern const int edgeFeatureNum = 2;
 extern const int surfFeatureNum = 4;
@@ -124,32 +123,33 @@ extern const float edgeThreshold = 0.1;
 extern const float surfThreshold = 0.1;
 extern const float nearestFeatureSearchSqDist = 25;
 
-
 // Mapping Params
 extern const float surroundingKeyframeSearchRadius = 50.0; // key frame that is within n meters from current pose will be considerd for scan-to-map optimization (when loop closure disabled)
-extern const int   surroundingKeyframeSearchNum = 50; // submap size (when loop closure enabled)
+extern const int surroundingKeyframeSearchNum = 50;        // submap size (when loop closure enabled)
 // history key frames (history submap for loop closure)
 extern const float historyKeyframeSearchRadius = 7.0; // key frame that is within n meters from current pose will be considerd for loop closure
-extern const int   historyKeyframeSearchNum = 25; // 2n+1 number of hostory key frames will be fused into a submap for loop closure
+extern const int historyKeyframeSearchNum = 25;       // 2n+1 number of hostory key frames will be fused into a submap for loop closure
 extern const float historyKeyframeFitnessScore = 0.3; // the smaller the better alignment
 
 extern const float globalMapVisualizationSearchRadius = 500.0; // key frames with in n meters will be visualized
 
-
-struct smoothness_t{ 
+struct smoothness_t
+{
     float value;
     size_t ind;
 };
 
-struct by_value{ 
-    bool operator()(smoothness_t const &left, smoothness_t const &right) { 
+struct by_value
+{
+    bool operator()(smoothness_t const &left, smoothness_t const &right)
+    {
         return left.value < right.value;
     }
 };
 
 /*
-    * A point cloud type that has "ring" channel
-    */
+ * A point cloud type that has "ring" channel
+ */
 struct PointXYZIR
 {
     PCL_ADD_POINT4D
@@ -158,15 +158,12 @@ struct PointXYZIR
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 
-POINT_CLOUD_REGISTER_POINT_STRUCT (PointXYZIR,  
-                                   (float, x, x) (float, y, y)
-                                   (float, z, z) (float, intensity, intensity)
-                                   (uint16_t, ring, ring)
-)
+POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZIR,
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(uint16_t, ring, ring))
 
 /*
-    * A point cloud type that has 6D pose info ([x,y,z,roll,pitch,yaw] intensity is time stamp)
-    */
+ * A point cloud type that has 6D pose info ([x,y,z,roll,pitch,yaw] intensity is time stamp)
+ */
 struct PointXYZIRPYT
 {
     PCL_ADD_POINT4D
@@ -178,13 +175,9 @@ struct PointXYZIRPYT
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;
 
-POINT_CLOUD_REGISTER_POINT_STRUCT (PointXYZIRPYT,
-                                   (float, x, x) (float, y, y)
-                                   (float, z, z) (float, intensity, intensity)
-                                   (float, roll, roll) (float, pitch, pitch) (float, yaw, yaw)
-                                   (double, time, time)
-)
+POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZIRPYT,
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(float, roll, roll)(float, pitch, pitch)(float, yaw, yaw)(double, time, time))
 
-typedef PointXYZIRPYT  PointTypePose;
+typedef PointXYZIRPYT PointTypePose;
 
 #endif
